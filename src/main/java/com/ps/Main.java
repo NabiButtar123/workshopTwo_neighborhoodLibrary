28914c287b62f7d06a2974fbf1b185aaac81c539
NabiButtar123 <72283253+NabiButtar123@users.noreply.github.com>
2025-04-20 21:41:51 -0400

    added second case with many different excep, tricky scanner issue with next line

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -7,7 +7,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-
         Book[] books = {
                 new Book(1, "9780143127741", "To Kill a Mockingbird"),
                 new Book(2, "9780743273565", "The Great Gatsby"),
@@ -31,7 +30,6 @@ public class Main {
                 new Book(20, "9781982137274", "Verity")
         };
 
-
         boolean programRunning = true;
 
         while (programRunning) {
@@ -40,27 +38,33 @@ public class Main {
             System.out.println("Hello! enter 1 to Show Available books");
             System.out.println("Please enter 2 to Show Checked Out Books");
             System.out.println("Please enter 3 to exit the Library");
+            System.out.print("Please enter your choice: ");
 
             int input = scanner.nextInt();
+            scanner.nextLine();
+
+
             switch (input) {
                 case 1:
                     System.out.println("There are the available books!");
                     for (Book b : books) {
                         if (!b.isCheckedOut()) {
-                            System.out.println("ID: " + b.getId() + ", ISBN: " + b.getIsbn() + ", Title0: " + b.getTitle());
+                            System.out.println("ID: " + b.getId() + ", ISBN: " + b.getIsbn() + ", Title: " + b.getTitle());
                         }
                     }
 
                     boolean stayBookMenu = true;
 
                     while (stayBookMenu) {
-                        System.out.println("Please enter the book id you would like to check out or enter 0 ");
+                        System.out.println("\nPlease enter the book id you would like to check out or enter 0 ");
                         System.out.println("to go back to the home screen");
+                        System.out.print("Please enter id or 0: ");
                         int checkOutID = scanner.nextInt();
+                        scanner.nextLine();
 
+                        // mistake: doesn't check out from here, i can put this into if statement
                         if (checkOutID == 0) {
                             stayBookMenu = false;
-
                         }
 
                         if (checkOutID > 0 && checkOutID <= books.length) {
@@ -69,101 +73,103 @@ public class Main {
                                 System.out.print("Enter your name: ");
                                 String name = scanner.nextLine();
                                 books[checkOutID - 1].checkOut(name);
-                                System.out.println("Book checked out!");
+                                System.out.println("Book checked out!\n");
                             } else {
-                                System.out.println("Book is already checked out.");
+                                System.out.println("Book is already checked in!\n");
                             }
                         } else {
-                            System.out.println("invalid book id");
+                            System.out.println("invalid book id\n");
+                        }
+
+                        System.out.println("Would you like to check  out another book? Enter as so: ");
+                        System.out.println("1. Check out another book");
+                        System.out.println("2. Return to the home screen\n");
+                        System.out.print("Enter your choice: ");
+                        int nextChoice = scanner.nextInt();
+
+
+                        switch (nextChoice) {
+                            case 1:
+                                // Show ID prompt again, but not full list (for cleanness)
+                                stayBookMenu = true;
+                                break;
+                            case 2:
+                                System.out.println("Returning to the home screen \n");
+                                stayBookMenu = false;
+                                break;
+                            default:
+                                System.out.println("Invalid choice, returning to main menu\n");
+                                stayBookMenu = false;
+                                break;
                         }
                     }
-                    System.out.println("Would you like to check another book? Enter as so: ");
-                    System.out.println("1. Check out another book");
-                    System.out.println("2. Return to the home screen");
-                    System.out.print("Enter your choice: ");
-                    int nextChoice = scanner.nextInt();
+                    break;
 
-                    switch (nextChoice) {
-                        case 1:
-                            // Show ID prompt again, but not full list
-                            stayBookMenu = true;
+                case 2: //showing checked out books
+                    System.out.println("Checked Out Books:");
+                    for (Book b : books) {
+                        if (b.isCheckedOut()) {
+                            System.out.println("ID: " + b.getId() + ", ISBN: "
+                                    + b.getIsbn() + ", Title: " + b.getTitle() + ", Checked Out To: " + b.getCheckedOutTo());
+                        }
+                    }
+                    System.out.println("Please enter 'C' to check in a book ");
+                    System.out.println("Please enter 'X' to go back to home screen");
+                    System.out.print("Please enter your choice: ");
+
+
+                    String inputCheckIn = scanner.nextLine().trim().toUpperCase();
+                    switch (inputCheckIn) {
+                        case "C":
+                            boolean stayCheckIn = true;
+
+                            while (stayCheckIn) {
+                                System.out.print("Please enter book id: ");
+                                int checkInID = scanner.nextInt();
+                                scanner.nextLine();
+
+
+                                if (checkInID == 0 && checkInID <= books.length) {
+                                    System.out.println("Returning to the home screen \n");
+                                    stayCheckIn = false;
+                                    break;
+                                }
+
+                                if (checkInID > 0 && checkInID <= books.length) {
+                                    Book checkInBook = books[checkInID - 1];
+                                    if (checkInBook.isCheckedOut()) {
+                                        checkInBook.checkIn();
+                                        System.out.println("Book has been checked in!\n");
+                                    } else {
+                                        System.out.println("That book is already checked in\n");
+                                    }
+                                    System.out.print("Enter 'C' to check in another book or 'X' to return to main menu: ");
+
+                                    String anotherInputCheck = scanner.next().toUpperCase();
+
+                                    if (anotherInputCheck.equals("C")) {
+                                        stayCheckIn = true;
+                                    } else {
+                                        stayCheckIn = false;
+                                        System.out.println("Returning to main menu\n");
+                                    }
+                                }
+                            }
                             break;
-                        case 2:
-                            System.out.println("Returning to the home screen ");
-                            stayBookMenu = false;
+                        case "X":
+                            System.out.println("Returning to main menu ");
                             break;
                         default:
-                            System.out.println("Invalid choice, returning to main menu");
-                            stayBookMenu = false;
+                            System.out.println("invalid input going back to home screen");
                             break;
                     }
-            }
-            break;
+                    break;
 
-            case 2: //showing checked out books
-                System.out.println("Checked Out Books:");
-                for (Book b : books) {
-                    if (b.isCheckedOut()) {
-                        System.out.println("ID: " + b.getId() + ", ISBN: "
-                                + b.getIsbn() + ", Title: " + b.getTitle() + ", Checked Out To: " + b.getCheckedOutTo());
-                    }
-                }
-                System.out.println("Please enter 'C' to check in a book ");
-                System.out.println("Please enter 'X' to go back to home screen");
-                char inputCheckin = scanner.nextLine().toUpperCase().charAt(0);
-
-                switch (inputCheckin) {
-                    case 'C':
-                        boolean stayCheckIn = true;
-
-                        while (stayCheckIn) {
-                            System.out.println("Please enter book id");
-                            int checkInID = scanner.nextInt();
-
-                            if (checkInID == 0 && checkInID <= books.length) {
-                                System.out.println("Returning to the home screen ");
-                                stayCheckIn = false;
-                                break;
-                            }
-
-                            if (checkInID > 0 && checkInID <= books.length) {
-                                Book checkInBook = books[checkInID - 1];
-                                if (checkInBook.isCheckedOut()) {
-                                    checkInBook.checkIn();
-                                    System.out.println("Book has been checked in!");
-                                } else {
-                                    System.out.println("That book is not checked in");
-                                }
-                                System.out.println("Enter 'C' to check in another book or 'X  to return to main menu");
-
-                                char anotherInputCheck = scanner.nextLine().toUpperCase().charAt(0);
-
-                                if (anotherInputCheck == 'C') {
-                                    stayCheckIn = true;
-                                } else {
-                                    stayCheckIn = false;
-                                    System.out.println("Returning to main menu");
-                                }
-                            }
-                        }
-                        break;
-                    case 'X':
-                        System.out.println("Returning to main menu ");
-                        break;
-                    default:
-                        System.out.println("invalid input going back to home screen");
-                        break;
-                }
-                break;
-            case 3:
-                programRunning = false;
-                System.out.println("Thank you for visiting the library please come again soon!");
-                break;
-
-                        }
+                case 3:
+                    programRunning = false;
+                    System.out.println("Thank you for visiting the library please come again soon!");
+                    break;
             }
         }
     }
 }
-
-
